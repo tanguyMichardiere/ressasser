27d90570e54c4db2ab434e503d883923f2e46fcd
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2025-11-28 18:34:07 +0100

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
 				<div className="max-w-md">
 					<h1 className="text-5xl font-bold">Not Found</h1>
 					<p className="py-6">Could not find the requested page.</p>
-					<Link className="link" href="/">
+					<Link className="link" href="/feed">
 						Home Page
 					</Link>
 				</div>
